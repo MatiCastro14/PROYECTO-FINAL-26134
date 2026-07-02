81a3f471cb2ee52e96fd561eb5d2892c19d8429b
informaticaSHN <167133358+MatiCastro14@users.noreply.github.com>
2026-07-02 13:58:24 -0300

    cam index.js

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 import 'dotenv/config';
-import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app.js';
 
-
-app.use("/api/products", productsRouter);
-
-
-import authRouter from './src/routes/auth.router.js';
-app.use("/api/auth", authRouter);
-
-const app = express()
-
-app.use("/api/products", productsRouter);
-
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => 'http://localhost:${PORT}');
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
