c85c1c2790fbfa2015da3d9ab71d4abe918d31ef
informaticaSHN <167133358+MatiCastro14@users.noreply.github.com>
2026-07-06 10:01:47 -0300

    cambios en el token generator

--- src/utils/token.generator.js
+++ src/utils/token.generator.js
@@ -1,13 +1,12 @@
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-
+import jwt from "jsonwebtoken";
 export const generateToken = (userData) => {
   const payload = {
     id: userData.id,
   };
+console.log("La clave secreta actual es:", process.env.JWT_SECRET);
 
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
