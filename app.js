4c6e9201516977e7aa9dadf3098f7cc48d6da0a5
MatiCastro14 <mcm31415@gmail.com>
2026-07-09 19:44:49 -0300

    cambios

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ dotenv.config();
 
 
 import express from "express";
-
+import cors from "cors";
+import categoriesRouter from "./src/routes/categories.router.js";
+import usersRouter from "./src/routes/users.router.js";
 import motosRouter from "./src/routes/motos.router.js";
 import authRouter from "./src/routes/auth.router.js";
 import { authentication as auth } from "./src/middlewares/auth.middleware.js";
@@ -13,8 +15,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-    res.json({ message: "Bienvenidos a la API RESTfull" });
+  res.json({
+    message: "Servidor funcionando correctamente",
+  });
 });
 
 
@@ -29,8 +35,17 @@ app.use("/api/auth", authRouter);
 app.use("/api/categories", categoriesRouter);
 app.use("/api/users", usersRouter);
 
+//Servicio de estado del servidor
+app.get("/up", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Servidor activo",
+  });
+});
 
-
+app.use((req, res) => {
+  res.status(404).json({ error: "Ruta no encontrada" });
+});
 
 
 export default app;
